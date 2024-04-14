e02e4829ace44ac5a3b5920bb1105f080b2b46cb
Evan <evanz1215@gmail.com>
2024-04-14 19:59:01 +0800

    使用flutter的隱式動畫元件

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -32,7 +32,22 @@ class MealDetailsScreen extends ConsumerWidget {
                   ),
                 );
               },
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(
+                    turns: Tween<double>(
+                      begin: 0.8,
+                      end: 1,
+                    ).animate(animation), // animation,使用預設animation會從0~1完整轉，可以改用Tween()
+                    child: child,
+                  );
+                },
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
+                ),
+              ),
             )
           ],
         ),
